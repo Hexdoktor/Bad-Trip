d8b4d4c39cc9a4997471a499c71238d6dccbf65a
susadebeast <126854182+susadebeast@users.noreply.github.com>
2023-03-24 21:14:06 +0200

    allows setting stepheight, dropheight, waitforplayer + actdistance, allowjump for the enemies in unity editor

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -6,6 +6,14 @@ public class EnemyMover : MonoBehaviour
 
     public float moveSpeed;
     public float jumpForce;
+    public float stepheight;
+    public float dropheight;
+    public bool waitforplayer; // doesn't move until player is close enough (or moves, but doesn't step up / drop down), is set to false when player got close enough
+    public float actdistance; // activation distance
+    public bool allowjump; // the enemy can jump randomly, though suppose just setting the jumpForce to non zero would allow jumping
+
+    bool landed;
+
     float move_time;
     float move_dir; // 0 = right, 1 = left, 2 = vertical (vertical may be a test/placeholder for attack)
 
@@ -14,9 +22,10 @@ public class EnemyMover : MonoBehaviour
     private Collider2D hitbox;
     private RaycastHit2D hit;
 
-    public Transform groundCheck;
+    public Transform pos;
     public LayerMask Ground;
-    public LayerMask invisiblock;
+    public LayerMask Player;
+
 
     void ChangeMoveDir()
     {
@@ -29,10 +38,11 @@ public class EnemyMover : MonoBehaviour
     void CheckMoveDir()
     {
         // check for a dropoff down
-        Vector2 vec = new Vector2(0, 2.5f);
+        Vector2 vec = new Vector2(0, dropheight);
         hit = Physics2D.Linecast(rb.position, rb.position - vec, Ground);
         if(!hit) {
-            ChangeMoveDir();
+            if(Time.timeSinceLevelLoad > move_time)
+                ChangeMoveDir();
             return;
         }
         // on a slope
@@ -43,11 +53,11 @@ public class EnemyMover : MonoBehaviour
         else
             vec = new Vector2(1.0f, 0.0f);
         // hit something
-        if(Physics2D.CapsuleCast(rb.position, size*0.95f, CapsuleDirection2D.Vertical, 0, vec, 0.05f, Ground)) {
+        if(Physics2D.CapsuleCast(rb.position, size*0.95f, CapsuleDirection2D.Vertical, 0, vec, stepheight, Ground)) {
             // can be stepped up?
-            if(!Physics2D.CapsuleCast(rb.position + new Vector2(0, 0.6f), size*0.95f, CapsuleDirection2D.Vertical, 0, vec, 0.05f, Ground)) {
+            if(!Physics2D.CapsuleCast(rb.position + new Vector2(0, stepheight + 0.1f), size*0.95f, CapsuleDirection2D.Vertical, 0, vec, stepheight, Ground)) {
                 if(slopeAngle == 0)
-                    transform.Translate(0, 0.6f, 0);
+                    transform.Translate(0, stepheight + 0.1f, 0);
             }
             else
                 ChangeMoveDir();
@@ -56,25 +66,19 @@ public class EnemyMover : MonoBehaviour
 
     void MoveEnemy()
     {
-        // just vertical movement
-        if (Time.timeSinceLevelLoad > move_time && move_dir == 2 && IsGrounded()) {
-            ChangeMoveDir();
-            //move_time = Time.timeSinceLevelLoad + 0.5f + Random.value*4.5f;
-        }
         // left
-        if (move_dir == 0) {
+        if(move_dir == 0) {
             sidemove = -moveSpeed * Time.deltaTime;
             //transform.localScale = new Vector3(-0.35f, 0.35f, 1); // kääntää Spriten toiseen suuntaan
-
-            transform.Translate(sidemove, 0, 0);
         }
         // right
-        if (move_dir == 1) {
+        else {
             sidemove = moveSpeed * Time.deltaTime;
             //transform.localScale = new Vector3(0.35f, 0.35f, 1); // kääntää Spriten toiseen suuntaan
-
-            transform.Translate(sidemove, 0, 0);
         }
+        //if(!landed)
+        //    sidemove *= 5;
+        transform.Translate(sidemove, 0, 0);
     }
 
     bool IsGrounded()
@@ -90,8 +94,15 @@ public class EnemyMover : MonoBehaviour
 
     void JumpEnemy()
     {
-        //if(Random.value < 0.05f)// && IsGrounded())
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        move_time = Time.timeSinceLevelLoad + 2.5f;
+        landed = false;
+    }
+    
+
+    bool FindPlayer()
+    {
+        return (Physics2D.OverlapCircle(rb.position, actdistance, Player));
     }
 
 
@@ -99,24 +110,44 @@ public class EnemyMover : MonoBehaviour
     {
         hitbox = GetComponent<Collider2D>();
         size = hitbox.bounds.size;
+        // default values if nothing set in Unity
+        if(stepheight == 0)
+            stepheight = 0.05f;
+        if(dropheight == 0)
+            dropheight = 2.5f;
+        if(actdistance < 5f)
+            actdistance = 5f;
+        landed = true;
     }
 
 
     void Update()
     {
-        CheckMoveDir();
-        MoveEnemy();
-        //JumpEnemy();
+        // move around
+        if(waitforplayer == false) {
+            CheckMoveDir();
+            MoveEnemy();
+            // this enemy can jump
+            if(allowjump && Random.value < 0.01f && IsGrounded() && Time.timeSinceLevelLoad > move_time + 0.5f)
+                JumpEnemy();
+        }
+        // wait for player to get close enough
+        else {
+            if(FindPlayer())
+                waitforplayer = false;
+        }
     }
 
-    // hits an invisible trigger
-    void OnTriggerEnter2D(Collider2D col)
+
+    // hits an invisible trigger, not currently in use, would need to check for specific trigger?
+    void OnTriggerEnter2D_notused(Collider2D col)
     {
         if (move_dir == 0)
             move_dir = 1;
         else
             move_dir = 0;
     }
+
 
     void OnCollisionEnter2D(Collision2D col)
     {
@@ -136,8 +167,11 @@ public class EnemyMover : MonoBehaviour
             transform.Translate(-sidemove, 0, 0);
             //col.gameObject.SendMessage("ApplyDamage", 1);
         }
+        ChangeMoveDir();
+        /*
         move_dir = 2;
         move_time = Time.timeSinceLevelLoad + 0.25f;
         JumpEnemy();
+        */
     }
 }
