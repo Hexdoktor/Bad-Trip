c67ab2fc4a0265bc7c9174ea61e1990d92865786
Juuso Savinainen <savinainenjjj@gmail.com>
2023-05-16 18:28:42 +0300

    Pieni muutos ja lisäys
    Hp itemille ääniefekti

--- Assets/Scripts/UI/HealthItem.cs
+++ Assets/Scripts/UI/HealthItem.cs
@@ -12,10 +12,13 @@ public class HealthItem : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
+
             PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
             
             playerHealth.currentHealth += healthAmount;
             
+            FindObjectOfType<AudioManager>().Play("NomNom");
+
             playerHealth.currentHealth = Mathf.Clamp(playerHealth.currentHealth, 0, playerHealth.maxHealth);
             
             playerHealth.healthBar.SetHealth(playerHealth.currentHealth);
