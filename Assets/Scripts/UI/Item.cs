c8b22521bf41c370399128290f736f609de6d838
Mila <turpeinenmila6@gmail.com>
2023-12-14 00:39:44 +0200

    Collectible progression visible

--- Assets/Scripts/UI/Item.cs
+++ Assets/Scripts/UI/Item.cs
@@ -2,18 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour, ICollectible
 {
+    [SerializeField] GameObject collectibleUI;
+    [SerializeField] Sprite newSprite;
 
     public static event Action OnItemCollected;
-   
-        
+
+
     public void Collect()
     {
         Debug.Log("Collected Item");
+        collectibleUI.GetComponent<Image>().sprite = newSprite;
+
         FindObjectOfType<AudioManager>().Play("ItemCollect");
         Destroy(gameObject);
         OnItemCollected?.Invoke();
+
     }
 }
+
